80cf9fceb8de9af1e868d309cba17c11e9ab89a6
fdmysterious@gmail.com <fdmysterious@gmail.com@7dcd1880-0a31-fec2-e613-19d149a1c63a>
2011-07-11 21:41:07 +0000

    ajout des premiers fichiers de l'exemple PONG.

---
+++ trunk/ omega-engine/examples/pong/headers/application.hpp
@@ -0,0 +1,14 @@
+/*********************
+ *-=application.hpp=-*
+ *********************
+ 
+ Auteur : My?terious
+ Description : Classe principale qui gère l'application.
+*/
+#ifndef HEADER_APPLICATION
+#define HEADER_APPLICATION
+
+//-=Inclusion des headers=-//
+//-=Fin de la section=-//
+
+#endif
